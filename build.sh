c08536f645318bbf4fd1e59b499c2ec86bb29e73
sagardwivedi <sagar.dw20@gmail.com>
2025-03-16 20:47:57 +0000

    configure for deployment

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Add uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 uv pip sync requirements.txt
 
