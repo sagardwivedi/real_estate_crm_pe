2a3bab1ca56744e189ba31cd5e4e716e2f8a9928
sagardwivedi <sagar.dw20@gmail.com>
2025-03-16 21:06:54 +0000

    configure for deployment

--- build.sh
+++ build.sh
@@ -2,20 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
-# Ensure uv is in the PATH
-export PATH="/opt/render/.local/bin:$PATH"
+# Convert static asset files
+python manage.py collectstatic --no-input
 
-# Persist PATH change for future shell sessions
-echo 'export PATH="/opt/render/.local/bin:$PATH"' >> $HOME/.profile
-
-# Install dependencies
-uv pip sync requirements.txt
-
-# Collect static files
-uv run python manage.py collectstatic --no-input
-
-# Apply database migrations
-uv run python manage.py migrate
+# Apply any outstanding database migrations
+python manage.py migrate
